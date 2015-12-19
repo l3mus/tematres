b944d5e23e91127acc4159124aaf13b934de7309
Dimitar Georgiev <dimiter_georgiev@mail.bg>
2015-12-19 02:19:22 -0800

    Made needed Modifications to work with TemaTres

--- plugins/core/init.php
+++ plugins/core/init.php
@@ -1,6 +1,6 @@
 <?php
-session_start();
-
+//session_start();
+//echo 'Init included';
 $GLOBALS['config'] = array(
     'mysql' => array(
         'host' => '127.0.0.1',
@@ -11,21 +11,21 @@ $GLOBALS['config'] = array(
 );
 
 //spl_autoload_register(function($class){
-//    require_once '../Plugin_classes/'.$class.'.php'; //the path depends on the calling file location - in this case user_menu_test/prototype.php
+//    require_once '../plugins/Plugin_classes/'.$class.'.php'; //the path depends on the calling file location - in this case user_menu_test/prototype.php
 //});
 
-spl_autoload_register(function($className){
-    $className = ltrim($className, '\\');
-    $fileName = '';
-    $namespace = '';
-    if($lastNsPos = strrpos($className, '\\')){ //find the last occurrence
-        $namespace = substr($className, 0, $lastNsPos);
-        $className = substr($className, $lastNsPos + 1);
-        $fileName = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-    }
-    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
-    require_once $fileName;
-    //echo $fileName;
-});
+//spl_autoload_register(function($className){
+//    $className = ltrim($className, '\\');
+//    $fileName = '';
+//    $namespace = '';
+//    if($lastNsPos = strrpos($className, '\\')){ //find the last occurrence
+//        $namespace = substr($className, 0, $lastNsPos);
+//        $className = substr($className, $lastNsPos + 1);
+//        $fileName = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+//    }
+//    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+//    require_once $fileName;
+//    //echo $fileName;
+//});
 
-require_once 'functions/sanitize.php';
+//require_once 'functions/sanitize.php'; //check the path!!!
--- plugins/plugin_classes/DB.php
+++ plugins/plugin_classes/DB.php
@@ -10,15 +10,46 @@ class DB{
     private function __construct(){
         try{
             $this->_pdo = new \PDO('mysql:host='. Config::get('mysql/host') .';dbname='. Config::get('mysql/db') , Config::get('mysql/username'), Config::get('mysql/password'));
-            echo 'Connected!';
+            //echo 'Connected!';
         }catch(\PDOException $e){
             exit($e->getMessage());
         }
     }
-    public static function getInstance(){
+    public static function getInstance(){ //single tone pattern
         if(!isset(self::$_instance)){
             self::$_instance = new DB();
         }
         return self::$_instance;
+    }
+    public function query($sql, $params = array()){
+        $this->_error = false;
+        if($this->_query = $this->_pdo->prepare($sql)){
+            $x = 1;
+            if(count($params)){
+                foreach($params as $param){
+                    $this->_query->bindValue($x, $param);
+                    $x++;
+                }
+            }
+            if($this->_query->execute()){ //this will fail only if we have wrong query, but not if the result is empty - for example wrong table name is an error but looking for non-existing value is not an error
+                $this->_results = $this->_query->fetchAll(\PDO::FETCH_OBJ); //To check if the value exists always check if $_count > 0 !!!
+                $this->_count = $this->_query->rowCount();
+            }else{
+                $this->_error = true;
+            }
+        }
+        return $this;
+    }
+    public function error(){
+        return $this->_error;
+    }
+    public function rowsCount(){
+        return $this->_count;
+    }
+    public function results(){
+        return $this->_results;
+    }
+    public static function test(){
+        echo 'Test from DB';
     }
 }
