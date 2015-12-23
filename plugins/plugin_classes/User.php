02ab958e648d5ca4c2e79967b08666873e05951c
Dimitar Georgiev <dimiter_georgiev@mail.bg>
2015-12-23 00:41:17 -0800

    Added inital code for User.php

---
+++ plugins/plugin_classes/User.php
@@ -0,0 +1,16 @@
+<?php
+session_start(); //This statement should be used only with Ajax
+use \Plugin_classes\DB as DBase;
+use \Plugin_classes\Config;
+class User{
+    private $_result;
+
+    public static function exists(){
+        if(isset($_POST['username']) && isset($_POST['password'])) {
+            if (DBase::getInstance()->query("select mail, pass form pr_usuario where mail = {$_POST['username']} and pass = {$_POST['password']}")) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
--- plugins/prototype.php
+++ plugins/prototype.php
@@ -1,23 +1,32 @@
 <?php
+session_start();
 require_once 'core/init.php';
 use \Plugin_classes\Config;
 use \Plugin_classes\DB;
+use \Plugin_classes\User;
 
-//echo Config::get('mysql/host');
+////echo Config::get('mysql/host');
+//
+//$user = DB::getInstance()->query("select nombres, apellido from pr_usuario where id = 7");
+//if(!$user->error()){
+//    if($user->rowsCount()){
+//        echo 'User exists<br>';
+//    }else{
+//        echo 'No such user<br>';
+//    }
+//}else{
+//    echo 'Wrong query<br>';
+//}
+//foreach($user->results() as $row){
+//    foreach($row as $item=>$value){
+//        echo $value . ' ';
+//    }
+//}
+////$user->error();
 
-$user = DB::getInstance()->query("select nombres, apellido from pr_usuario where id = 7");
-if(!$user->error()){
-    if($user->rowsCount()){
-        echo 'User exists<br>';
-    }else{
-        echo 'No such user<br>';
-    }
-}else{
-    echo 'Wrong query<br>';
-}
-foreach($user->results() as $row){
+foreach($_SESSION as $row){
     foreach($row as $item=>$value){
-        echo $value . ' ';
+        echo $value.'<br>';
     }
 }
-//$user->error();
+
