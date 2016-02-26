85a056c4077361b620c6a79ac2d968455c025194
Dimitar Georgiev <dimiter_georgiev@mail.bg>
2016-02-26 00:55:42 -0800

    plugins/js/userMenu.js -> Added login method with AJAX for authentication

--- plugins/js/userMenu.js
+++ plugins/js/userMenu.js
@@ -1,24 +1,24 @@
 plugins.userMenu = (function(){
     var configMap = {login_html : String() +
-                    '<div id="login-form"><form>' +
+                    '<div id="login-form"><form method="post" id="loginForm">' +
                         '<ul>' +
                             '<li>' +
     '                           <div class="flex-row">' +
     '                               <div class="row-label">' +
-    '                                   <span class="glyphicon glyphicon-user label-span"></span>' +
+    '                                   <label for="username"><span class="glyphicon glyphicon-user label-span"></span></label>' +
     '                               </div>' +
     '                               <div class="row-input">' +
-    '                                   <input class="plugin-input" type="text" placeholder="Username"/>' +
+    '                                   <input id="username" name="username" class="plugin-input" type="email" placeholder="Username" required/>' +
     '                               </div>' +
     '                           </div>' +
     '                       </li>' +
                             '<li>' +
     '                           <div class="flex-row">' +
     '                               <div class="row-label">' +
-    '                                   <span class="glyphicon glyphicon-lock label-span"></span>' +
+    '                                   <label for="password"><span class="glyphicon glyphicon-lock label-span"></span></label>' +
     '                               </div>' +
     '                               <div class="row-input">' +
-    '                                   <input class="plugin-input" type="password" placeholder="Password">' +
+    '                                   <input id="password" name="password" class="plugin-input" type="password" placeholder="Password" required/>' +
     '                               </div>' +
     '                           </div>' +
     '                       </li>' +
@@ -27,9 +27,9 @@ plugins.userMenu = (function(){
                     '</form></div>',
                      logged_html : '<div style="color:#000;">Logged in already!</div>',
                      logout_html : '',
-                     menu_extend_time : 1500,
+                     menu_extend_time : 800,
                      menu_retract_time : 500,
-                     menu_ht_extended : 180,
+                     menu_ht_extended : 220,
                      menu_ht_retracted : 0,
                      menu_extended_label : 'Click to retract',
                      menu_retracted_label : 'Click to extend'
@@ -80,6 +80,30 @@ plugins.userMenu = (function(){
         toggleMenu(stateMap.is_retracted);
         return false;
     };
+    var login = function(){
+        $(document).on("submit", "#loginForm", function(){
+            //alert('loginForm Test!@!');
+            //var sss = $("#id_password").val();
+            //alert(sss);
+            $.ajax({
+                type: "POST",
+                url: "../plugins/tempFunctionality/account.php",
+                data: {
+                    username: $("#username").val(),
+                    password: $("#password").val()
+                    //id_correo_electronico: $("#id_correo_electronico").val(),
+                    //id_password: $("#id_password").val()
+                },
+                success: function(response){
+                    alert(response);
+                    //if(response === 'redirect'){
+                    //    window.location.href = "index.php";
+                    //}
+                }
+            });
+            return false;
+        });
+    };
     var initModule = function($container, button, is_logged_in){
         stateMap.$container = $container;
         stateMap.button = button;
@@ -90,6 +114,7 @@ plugins.userMenu = (function(){
         //setTimeout(function(){toggleMenu(false)}, 8000);
         stateMap.is_retracted = true;
         jqueryMap.button.attr('title', configMap.menu_retracted_label).click(onClick);
+        login();
     };
     return {initModule : initModule};
 }());
