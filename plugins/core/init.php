58e24d461c8a828e465d83dcdd355104e701c2d9
Dimitar Georgiev <dimiter_georgiev@mail.bg>
2016-03-01 01:45:19 -0800

    plugins/core/init.php -> Fixed path problem related to the autoloader

--- plugins/core/init.php
+++ plugins/core/init.php
@@ -3,9 +3,13 @@
 //echo 'Init included';
 $GLOBALS['config'] = array(
     'mysql' => array(
+        //Please fill out all the credentials for database access
         'host' => '127.0.0.1',
+
         'username' => 'root',
+
         'password' => '',
+
         'db' => 'tematres'
     )
 );
@@ -13,8 +17,9 @@ $GLOBALS['config'] = array(
 spl_autoload_register(function($class){
     //require_once '../plugins/Plugin_classes/'.$class.'.php'; //the path depends on the calling file location - in this case user_menu_test/prototype.php
     //require_once '..'.DIRECTORY_SEPARATOR.'plugins'.DIRECTORY_SEPARATOR.$class.'.php';
-    require_once '..'.DIRECTORY_SEPARATOR.$class.'.php'; //This path works only for files located in tempFunctionality folder
-    //require_once '..' . DIRECTORY_SEPARATOR . 'Plugin_classes' . DIRECTORY_SEPARATOR . $class . '.php';
+    //require_once '..'.DIRECTORY_SEPARATOR.$class.'.php'; // Use locally!!! This path works only for files located in tempFunctionality folder
+    $classResolved = str_replace('\\', DIRECTORY_SEPARATOR, $class); // Fix path problem - unlv.dimitar-expertise.com Works together with the next line
+    require_once '..'.DIRECTORY_SEPARATOR.$classResolved.'.php'; // Fix path problem - unlv.dimitar-expertise.com
 }, $throw = true, $prepend = true);
 
 //spl_autoload_register(function($className){
